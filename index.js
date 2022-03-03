a477713356c9c9086e59ec6aa4fb4a1f8460c7a4
Tulasi Surapaneni <lakstul@gmail.com>
2022-03-02 22:00:11 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let count = 0
 let countEl = document.getElementById("count")
 
-function increment(){
+const increment = () => {
     count += 1;
     countEl.textContent = count;
 }
 
-function decrement(){
+const decrement = () => {
     count -= 1;
     countEl.textContent = count;
 }
 
-function save(){
+ const save = () => {
     let individualCount = document.getElementById("individual_count")
     let individualStr = count + " ===> ";
     individualCount.textContent += individualStr
